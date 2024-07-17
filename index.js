bb7f1f4d950ca013bd7704f253d2da54dbdb2887
prasannadhaygude1 <Dhaygude1507@gmail.com>
2024-07-17 19:35:33 +0530

    Added get method to fetched plant

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const app = express()
 const PORT = 5000
 app.use(express.json())
 
-const plants = []
+const plants = [
+    {
+        "id": 503,
+        "name": "Mango",
+        "category": "indoor",
+        "image": "https://th.bing.com/th/id/OIP.LYPvC0O42rOv3XAdUaHo-gHaE8?rs=1&pid=ImgDetMain",
+        "price": 150,
+        "description": "Mango plant"
+    }
+]
 
 app.post("/plant", (req, res)=>{
     const {
@@ -34,6 +43,15 @@ app.post("/plant", (req, res)=>{
             message : "New Plant Added Successfully"
         })
 
+})
+
+app.get("/plants", (req, res)=>{
+
+    res.json({
+        success : true,
+        data : plants,
+        message : "All Plants Fetched successfully"
+    })
 })
 
 app.listen(PORT, ()=>{
